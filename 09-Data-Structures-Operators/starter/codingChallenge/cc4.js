7eb4ff84d056f71b8b5b87e5f301bb4ed338c74c
Michael Barnett <michaelbarnett17@gmail.com>
2021-09-09 23:50:42 -0500

    completed more of the course

--- 09-Data-Structures-Operators/starter/codingChallenge/cc4.js
+++ 09-Data-Structures-Operators/starter/codingChallenge/cc4.js
@@ -1,7 +1,33 @@
-let text = [`underscore_case`, `first_name`, `Some_Variable`, `calculate_AGE`, `delayed_departure`];
+`use strict`;
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
 
-for (const i of text) {
-    let strArray = i.split(`_`);
-    let newString = strArray[0] + strArray[1][0].toUpperCase() + strArray[1].slice(1).toLowerCase();
-    console.log(newString);
-}
+/*
+underscore_case
+first_name
+Some_Variable
+ calculate_AGE
+delayed_departure
+*/
+
+document.querySelector(`button`).addEventListener(`click`, function () {
+    const input = document.querySelector(`textarea`).value;
+    const inputArray = input.split(`\n`);
+
+
+    for (let i = 0; i <= inputArray.length - 1; i++) {
+        iTrimmed = inputArray[i].trim();
+        // console.log(iTrimmed);
+        const strArray = iTrimmed.split(`_`);
+        const newString = strArray[0] + strArray[1][0].toUpperCase() + strArray[1].slice(1).toLowerCase();
+        const padString = newString.padEnd(20, ` `)
+        
+        let stringWithIcon = padString;
+        
+        for (let j = 0; j <= i; j++) {
+            stringWithIcon += `✅`;
+        }
+        console.log(stringWithIcon);
+    }
+});
+
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,32 +5,32 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
 
 // const arr = [2, 3, 4];
 // const arr2 = ['pizza', 'water', 'cellphone']
@@ -47,43 +47,44 @@ const restaurant = {
 //   console.log(item);
 // }
 
-const keys = Object.keys(restaurant);
-console.log(keys);
+// const keys = Object.keys(restaurant);
+// console.log(keys);
 
-const values = Object.values(restaurant);
-console.log(values);
+// const values = Object.values(restaurant);
+// console.log(values);
 
-const mySet = new Set([`Pasta`, `Pizza`, `Pizza`, 3, 4, 10, 5, 10]);
-console.log(mySet);
+// const mySet = new Set([`Pasta`, `Pizza`, `Pizza`, 3, 4, 10, 5, 10]);
+// console.log(mySet);
 
-const mySet2 = new Set(`mike`);
-console.log(mySet2);
+// const mySet2 = new Set(`mike`);
+// console.log(mySet2);
 
-//Convert Set to Array
+// //Convert Set to Array
 
-const myArray = [...mySet];
-console.log(myArray);
-
-
-const myrestaurant = new Map();
-myrestaurant.set(`name`, `McDonalds`);
-myrestaurant.set(`type`, `Fast food`);
-
-console.log(myrestaurant);
-
-myrestaurant.set(`open`, 7)
-.set(`close`, 22)
-.set('mascot', `ronald`)
-console.log(myrestaurant);
+// const myArray = [...mySet];
+// console.log(myArray);
 
 
-console.log(myrestaurant.get(`name`));
+// const myrestaurant = new Map();
+// myrestaurant.set(`name`, `McDonalds`);
+// myrestaurant.set(`type`, `Fast food`);
 
-const question = new Map([
-  [`question`, ``]
+// console.log(myrestaurant);
 
-])
+// myrestaurant.set(`open`, 7)
+// .set(`close`, 22)
+// .set('mascot', `ronald`)
+// console.log(myrestaurant);
 
-for (const [key, value] of myrestaurant) {
-  console.log(key, value);
-}
+
+// console.log(myrestaurant.get(`name`));
+
+// const question = new Map([
+//   [`question`, ``]
+
+// ])
+
+// for (const [key, value] of myrestaurant) {
+//   console.log(key, value);
+// }
+
